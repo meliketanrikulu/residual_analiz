005a05161daa1e749f9e3a7d49ad6ab0365b0418
meliketanrikulu <melike@leodrive.ai>
2023-10-25 16:38:53 +0300

    three pose calculation

--- include/residual_analiz/residual_analiz.hpp
+++ include/residual_analiz/residual_analiz.hpp
@@ -13,26 +13,38 @@
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include "std_msgs/msg/float64_multi_array.hpp"
 #include <std_msgs/msg/float32.hpp>
+#include <std_msgs/msg/string.hpp>
 #include <memory>
 
 class ResidualAnaliz : public rclcpp::Node {
 public:
     ResidualAnaliz();
     using pose_msg = geometry_msgs::msg::PoseStamped;
-    using ExactTimeSyncPolicy = message_filters::sync_policies::ApproximateTime<pose_msg, pose_msg>;
+    using ExactTimeSyncPolicy = message_filters::sync_policies::ApproximateTime<pose_msg, pose_msg,pose_msg>;
     using ExactTimeSynchronizer = message_filters::Synchronizer<ExactTimeSyncPolicy>;
     std::shared_ptr<ExactTimeSynchronizer> sync_ptr_;
 
-    message_filters::Subscriber<pose_msg> sub_twist_sync;
+    message_filters::Subscriber<pose_msg> sub_ekf_dr_1_;
     message_filters::Subscriber<pose_msg> sub_aw_ekf_sync;
+    message_filters::Subscriber<pose_msg> sub_ekf_dr_2_;
+
+
+
 private:
     void testCallback(const geometry_msgs::msg::PoseStamped::ConstSharedPtr & in_aw_ekf_msg,
-                      const geometry_msgs::msg::PoseStamped::ConstSharedPtr & sub_twist_sync) ;
+                      const geometry_msgs::msg::PoseStamped::ConstSharedPtr & in_ekf_dr_1_,
+                      const geometry_msgs::msg::PoseStamped::ConstSharedPtr & in_ekf_dr_2_) ;
 
 
-    rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pub_pose_with_only_twist;
-    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_absolute_error;
-    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr pub_difference_xyz_yaw;
+    rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pub_pose_with_only_dr1;
+    rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pub_pose_with_with_dr2;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_absolute_error_dr1;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_absolute_error_dr2;
+    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr pub_difference_dr1_xyz_yaw;
+    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr pub_difference_dr2_xyz_yaw;
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_ndt_status;
+
 };
 
 #endif  // RESIDUAL_ANALIZ__RESIDUAL_ANALIZ_H
--- src/residual_analiz.cpp
+++ src/residual_analiz.cpp
@@ -11,22 +11,31 @@
 ResidualAnaliz::ResidualAnaliz()
     : Node("ResidualAnaliz") {
 
-    pub_absolute_error = this->create_publisher<std_msgs::msg::Float32>("/analyzer/position_difference/absolute_error_xy", 10);
-    pub_difference_xyz_yaw = this->create_publisher<std_msgs::msg::Float64MultiArray>("/analyzer/difference/xyz_yaw",10);
-    pub_pose_with_only_twist = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("pub_pose_with_only_twist",10);
+    pub_absolute_error_dr2 = this->create_publisher<std_msgs::msg::Float32>("/analyzer/position_difference/dr2/EuclideanDistance_xy", 10);
+    pub_absolute_error_dr1 = this->create_publisher<std_msgs::msg::Float32>("/analyzer/position_difference/dr1/EuclideanDistance_xy", 10);
 
-    sub_twist_sync.subscribe(this,"/localization/pose_twist_fusion_filter/biased_pose_only_velocity",rclcpp::QoS{1}.get_rmw_qos_profile());
-    sub_aw_ekf_sync.subscribe(this, "/localization/pose_twist_fusion_filter/biased_pose", rclcpp::QoS{1}.get_rmw_qos_profile());
+    pub_difference_dr1_xyz_yaw = this->create_publisher<std_msgs::msg::Float64MultiArray>("/analyzer/difference/dr1/xyz_yaw", 10);
+    pub_difference_dr2_xyz_yaw = this->create_publisher<std_msgs::msg::Float64MultiArray>("/analyzer/difference/dr2/xyz_yaw", 10);
 
-    sync_ptr_.reset(new ExactTimeSynchronizer(ExactTimeSyncPolicy(10), sub_aw_ekf_sync, sub_twist_sync));
+    pub_pose_with_only_dr1 = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("pub_pose_dr1", 10);
+    pub_pose_with_with_dr2 = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("pub_pose_dr2", 10);
+
+    pub_ndt_status =this->create_publisher<std_msgs::msg::String>("/ndt_status",10);
+
+    sub_aw_ekf_sync.subscribe(this, "/localization/pose_twist_fusion_filter/pose", rclcpp::QoS{1}.get_rmw_qos_profile());
+    sub_ekf_dr_1_.subscribe(this, "/localization/pose_twist_fusion_filter/ekf_pose_dr", rclcpp::QoS{1}.get_rmw_qos_profile());
+    sub_ekf_dr_2_.subscribe(this, "/localization/pose_twist_fusion_filter/ekf_pose_dr_2_", rclcpp::QoS{1}.get_rmw_qos_profile());
+
+    sync_ptr_.reset(new ExactTimeSynchronizer(ExactTimeSyncPolicy(10), sub_aw_ekf_sync, sub_ekf_dr_1_, sub_ekf_dr_2_));
 
     sync_ptr_->registerCallback(
-            std::bind(&ResidualAnaliz::testCallback, this, std::placeholders::_1, std::placeholders::_2));
+            std::bind(&ResidualAnaliz::testCallback, this, std::placeholders::_1, std::placeholders::_2,std::placeholders::_3));
     std::cout<<"sadkjsjdhfsa"<<std::endl;
 }
 
 void ResidualAnaliz::testCallback(const geometry_msgs::msg::PoseStamped::ConstSharedPtr &in_aw_ekf_msg,
-                                  const geometry_msgs::msg::PoseStamped::ConstSharedPtr &in_twist_msg) {
+                                  const geometry_msgs::msg::PoseStamped::ConstSharedPtr &in_ekf_dr_1_,
+                                  const geometry_msgs::msg::PoseStamped::ConstSharedPtr &in_ekf_dr_2_) {
 
 
     tf2::Quaternion aw_ekf_quaternion;
@@ -35,58 +44,126 @@ void ResidualAnaliz::testCallback(const geometry_msgs::msg::PoseStamped::ConstSh
     double roll_aw_, pitch_aw_, yaw_aw_;
     tf2::Matrix3x3(aw_ekf_quaternion).getRPY(roll_aw_, pitch_aw_, yaw_aw_);
 
-    //create Twist only posewithcovstampedmsg
-    geometry_msgs::msg::PoseWithCovarianceStamped ekf_with_only_twist;
-    ekf_with_only_twist.header = in_twist_msg->header;
-    ekf_with_only_twist.pose.pose = in_twist_msg->pose;
+    //create dr1 posewithcovstampedmsg
+    geometry_msgs::msg::PoseWithCovarianceStamped ekf_dr1_;
+    ekf_dr1_.header = in_ekf_dr_1_->header;
+    ekf_dr1_.pose.pose = in_ekf_dr_1_->pose;
 
-    double position_difference_x = abs(in_aw_ekf_msg->pose.position.x - in_twist_msg->pose.position.x);
-    double position_difference_y = abs(in_aw_ekf_msg->pose.position.y - in_twist_msg->pose.position.y);
-    double position_difference_z = abs(in_aw_ekf_msg->pose.position.z - in_twist_msg->pose.position.z);
+    double position_difference_x = abs(in_aw_ekf_msg->pose.position.x - in_ekf_dr_1_->pose.position.x);
+    double position_difference_y = abs(in_aw_ekf_msg->pose.position.y - in_ekf_dr_1_->pose.position.y);
+    double position_difference_z = abs(in_aw_ekf_msg->pose.position.z - in_ekf_dr_1_->pose.position.z);
 
-    ekf_with_only_twist.pose.covariance[0]  = std::pow(position_difference_x,2);
-    ekf_with_only_twist.pose.covariance[7]  = std::pow(position_difference_y,2);
-    ekf_with_only_twist.pose.covariance[14] = std::pow(position_difference_z,2);
+    ekf_dr1_.pose.covariance[0]  = std::pow(position_difference_x, 2);
+    ekf_dr1_.pose.covariance[7]  = std::pow(position_difference_y, 2);
+    ekf_dr1_.pose.covariance[14] = std::pow(position_difference_z, 2);
 
 
-    tf2::Quaternion twist_quaternion;
-    tf2::fromMsg(in_twist_msg->pose.orientation, twist_quaternion);
+    tf2::Quaternion dr1_quaternion;
+    tf2::fromMsg(in_ekf_dr_1_->pose.orientation, dr1_quaternion);
 
-    double roll_twist_only_, pitch_twist_only_, yaw_twist_only_;
-    tf2::Matrix3x3(twist_quaternion).getRPY(roll_twist_only_, pitch_twist_only_, yaw_twist_only_);
+    double roll_dr1_only_, pitch_dr1_only_, yaw_dr1_only_;
+    tf2::Matrix3x3(dr1_quaternion).getRPY(roll_dr1_only_, pitch_dr1_only_, yaw_dr1_only_);
 
-    double roll_difference_in_radians  = abs(roll_twist_only_ - roll_aw_);
-    double pitch_difference_in_radians = abs(pitch_twist_only_ - pitch_aw_);
+    double roll_difference_in_radians  = abs(roll_dr1_only_ - roll_aw_);
+    double pitch_difference_in_radians = abs(pitch_dr1_only_ - pitch_aw_);
     double yaw_difference_in_radians;
-    if ((yaw_twist_only_ > 1.5708 && yaw_aw_ < -1.5708) ||  (yaw_twist_only_ < -1.5708 && yaw_aw_ > 1.5708)){
-        yaw_difference_in_radians = 6.28319 - abs(yaw_twist_only_ - yaw_aw_);
+    if ((yaw_dr1_only_ > 1.5708 && yaw_aw_ < -1.5708) || (yaw_dr1_only_ < -1.5708 && yaw_aw_ > 1.5708)){
+        yaw_difference_in_radians = 6.28319 - abs(yaw_dr1_only_ - yaw_aw_);
     }
     else {
-        yaw_difference_in_radians = abs(yaw_twist_only_ - yaw_aw_);
+        yaw_difference_in_radians = abs(yaw_dr1_only_ - yaw_aw_);
     }
-    ekf_with_only_twist.pose.covariance[21] = std::pow(roll_difference_in_radians, 2);
-    ekf_with_only_twist.pose.covariance[28] = std::pow(pitch_difference_in_radians, 2);
-    ekf_with_only_twist.pose.covariance[35] = std::pow(yaw_difference_in_radians, 2);
+    ekf_dr1_.pose.covariance[21] = std::pow(roll_difference_in_radians, 2);
+    ekf_dr1_.pose.covariance[28] = std::pow(pitch_difference_in_radians, 2);
+    ekf_dr1_.pose.covariance[35] = std::pow(yaw_difference_in_radians, 2);
 
-    pub_pose_with_only_twist->publish(ekf_with_only_twist);
+    pub_pose_with_only_dr1->publish(ekf_dr1_);
 
     auto difference_ = std::make_unique<std_msgs::msg::Float64MultiArray>();
 
     difference_->data = {position_difference_x,position_difference_y,position_difference_z,(yaw_difference_in_radians * 180 / M_PI)};
     if((yaw_difference_in_radians * 180 / M_PI) >= 300.0){
-        std::cout<<"yaw_twist_only_ : "<<yaw_twist_only_ * 180 / M_PI <<std::endl;
+        std::cout << "yaw_dr1_only_ : " << yaw_dr1_only_ * 180 / M_PI << std::endl;
         std::cout<<"yaw_aw_ : "<<yaw_aw_ * 180 / M_PI <<std::endl;
         std::cout<<"yaw_difference_in_radians : : : "<<yaw_difference_in_radians<<std::endl;
     }
 
 
-    pub_difference_xyz_yaw->publish(std::move(difference_));
+    pub_difference_dr1_xyz_yaw->publish(std::move(difference_));
 
-    std_msgs::msg::Float32 absolute_error_msg;
-    absolute_error_msg.data = sqrt(std::pow((position_difference_x), 2) +
-                                   std::pow((position_difference_y), 2));
+    std_msgs::msg::Float32 absolute_error_dr1_msg;
+    absolute_error_dr1_msg.data = sqrt(std::pow((position_difference_x), 2) +
+                                       std::pow((position_difference_y), 2));
 
-    pub_absolute_error->publish(absolute_error_msg);
+    pub_absolute_error_dr1->publish(absolute_error_dr1_msg);
+
+
+    //2. compare pose
+
+//    tf2::Quaternion aw_ekf_quaternion;
+//    tf2::fromMsg(in_aw_ekf_msg->pose.orientation, aw_ekf_quaternion);
+//
+//    double roll_aw_, pitch_aw_, yaw_aw_;
+//    tf2::Matrix3x3(aw_ekf_quaternion).getRPY(roll_aw_, pitch_aw_, yaw_aw_);
+
+    //create dr1 only posewithcovstampedmsg
+    geometry_msgs::msg::PoseWithCovarianceStamped ekf_dr2_;
+    ekf_dr2_.header = in_ekf_dr_2_->header;
+    ekf_dr2_.pose.pose = in_ekf_dr_2_->pose;
+
+    double position_difference_dr2_x = abs(in_aw_ekf_msg->pose.position.x - in_ekf_dr_2_->pose.position.x);
+    double position_difference_dr2_y = abs(in_aw_ekf_msg->pose.position.y - in_ekf_dr_2_->pose.position.y);
+    double position_difference_dr2_z = abs(in_aw_ekf_msg->pose.position.z - in_ekf_dr_2_->pose.position.z);
+
+    ekf_dr2_.pose.covariance[0]  = std::pow(position_difference_dr2_x, 2);
+    ekf_dr2_.pose.covariance[7]  = std::pow(position_difference_dr2_y, 2);
+    ekf_dr2_.pose.covariance[14] = std::pow(position_difference_dr2_z, 2);
+
+    tf2::Quaternion dr2_quaternion;
+    tf2::fromMsg(in_ekf_dr_2_->pose.orientation, dr2_quaternion);
+
+    double roll_with_dr2_, pitch_with_dr2_, yaw_with_dr2_;
+    tf2::Matrix3x3(dr2_quaternion).getRPY(roll_with_dr2_, pitch_with_dr2_, yaw_with_dr2_);
+
+    double roll_difference_in_radians_dr2_  = abs(roll_with_dr2_ - roll_aw_);
+    double pitch_difference_in_radians_dr2_ = abs(pitch_with_dr2_ - pitch_aw_);
+    double yaw_difference_in_radians_dr2_;
+    if ((yaw_with_dr2_ > 1.5708 && yaw_aw_ < -1.5708) || (yaw_with_dr2_ < -1.5708 && yaw_aw_ > 1.5708)){
+        yaw_difference_in_radians_dr2_ = 6.28319 - abs(yaw_with_dr2_ - yaw_aw_);
+    }
+    else {
+        yaw_difference_in_radians_dr2_ = abs(yaw_with_dr2_ - yaw_aw_);
+    }
+    ekf_dr2_.pose.covariance[21] = std::pow(roll_difference_in_radians_dr2_, 2);
+    ekf_dr2_.pose.covariance[28] = std::pow(pitch_difference_in_radians_dr2_, 2);
+    ekf_dr2_.pose.covariance[35] = std::pow(yaw_difference_in_radians_dr2_, 2);
+
+    pub_pose_with_with_dr2->publish(ekf_dr2_);
+
+    auto difference_dr2_ = std::make_unique<std_msgs::msg::Float64MultiArray>();
+
+    difference_dr2_->data = {position_difference_dr2_x, position_difference_dr2_y, position_difference_dr2_z, (yaw_difference_in_radians_dr2_ * 180 / M_PI)};
+    if((yaw_difference_in_radians_dr2_ * 180 / M_PI) >= 300.0){
+        std::cout << "yaw_dr1_only_ : " << yaw_with_dr2_ * 180 / M_PI << std::endl;
+        std::cout<<"yaw_aw_ : "<<yaw_aw_ * 180 / M_PI <<std::endl;
+        std::cout << "yaw_difference_in_radians_dr2_ : : : " << yaw_difference_in_radians_dr2_ << std::endl;
+    }
+
+
+    pub_difference_dr2_xyz_yaw->publish(std::move(difference_dr2_));
+
+    std_msgs::msg::Float32 absolute_error_dr2_msg;
+    absolute_error_dr2_msg.data = sqrt(std::pow((position_difference_dr2_x), 2) +
+                                       std::pow((position_difference_dr2_y), 2));
+
+    pub_absolute_error_dr2->publish(absolute_error_dr2_msg);
+
+
+    if (absolute_error_dr1_msg.data >= 1.0 || absolute_error_dr2_msg.data >= 1.0){
+        std_msgs::msg::String ndt_status_;
+        ndt_status_.data = "NDT FAILED";
+        pub_ndt_status->publish(ndt_status_);
+    }
 
 }
 
